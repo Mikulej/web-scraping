455047d70674bccf3ff9ae000929c5bcfe946356
mj <mikulej7@gmail.com>
2024-07-20 00:25:12 +0200

    Find carousels

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from bs4 import BeautifulSoup
-
-#print(soup.prettify())
-
-import requests
+import requests as req
 
 #Get html from url
-url = 'https://steamcharts.com/'
-#url = 'https://bab.la/'
-#url = 'https://www.google.com/'
+url = 'https://store.steampowered.com/'
 #url = 'http://olympus.realpython.org/profiles/aphrodite'
-req = requests.Session()
+
 response = req.get(url)
 
 #Convert to soup object
-soup = BeautifulSoup(response.text, 'html.parser')
+#soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, 'lxml')
 
-print(soup.title)
+carousels = soup.findAll(class_='carousel_items responsive_scroll_snap_ctn')
+carousel = carousels[0]
+for link in carousel.findAll('a',class_="store_main_capsule responsive_scroll_snap_start broadcast_capsule"):
+    print("Featured game")
 
-#r = requests.get(url)
-# print("bab.la")
-#soup = BeautifulSoup(r.text, 'html.parser')
+carousel = carousels[1]
+for link in carousel.findAll('a',class_="store_capsule daily_deal"):
+    #for gameLink in link.findAll():
+    print("Capsule game")
+    #for gameLink in link.findAll('a'):
+        #print(gameLink)
+        #gameLink = link.find('a',href=True)
+    #print(link.prettify())
+#print(carousel)
 #print(soup.prettify())
-
-#print(soup.title)
