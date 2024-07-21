014f363aad544f1cc8224321df0091eecdc6ed8b
mj <mikulej7@gmail.com>
2024-07-21 16:27:18 +0200

    add JavaScript support (Selenium)

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 Create an app that is capable of fetching data from the internet for to user to use.  
 
 Uses **beautifulsoup4** https://pypi.org/project/beautifulsoup4/  
-Guide https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+Uses **Selenium** https://www.selenium.dev/documentation/webdriver/getting_started/first_script/  
+Guide for bs4 https://www.crummy.com/software/BeautifulSoup/bs4/doc/  
+Thoughts:
+Tool for fetching data must support js to web-scrap dynamicly. lib requests doesn't suppoer js
--- main.py
+++ main.py
@@ -1,28 +1,39 @@
 from bs4 import BeautifulSoup
-import requests as req
 
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
+driver = Chrome()
+url = 'https://avi.im/stuff/js-or-no-js.html' # test url
+#url = 'https://store.steampowered.com/'
+page = driver.get(url)
+p_element = driver.find_element(by=By.ID,value='intro-text')
+print(p_element.text)
+#soup = BeautifulSoup(page.text, 'lxml')
 #Get html from url
-url = 'https://store.steampowered.com/'
-#url = 'http://olympus.realpython.org/profiles/aphrodite'
+# url = 'https://store.steampowered.com/'
+# #url = 'http://olympus.realpython.org/profiles/aphrodite'
 
-response = req.get(url)
+# #Convert to soup object
+# #soup = BeautifulSoup(response.text, 'html.parser')
+# soup = BeautifulSoup(text, 'lxml')
+# featuredGames = soup.find_all('a',class_='store_main_capsule')
+# print(soup.prettify())
+# for link in featuredGames:
+#     print("Featured game")
+# carousels = soup.findAll(class_='carousel_items responsive_scroll_snap_ctn')
+# carousel = carousels[0]
+# # for link in carousel.findAll('a',class_="store_main_capsule responsive_scroll_snap_start broadcast_capsule"):
+# # #for link in carousel.findAll.a:
+# #     print("Featured game")
 
-#Convert to soup object
-#soup = BeautifulSoup(response.text, 'html.parser')
-soup = BeautifulSoup(response.text, 'lxml')
-
-carousels = soup.findAll(class_='carousel_items responsive_scroll_snap_ctn')
-carousel = carousels[0]
-for link in carousel.findAll('a',class_="store_main_capsule responsive_scroll_snap_start broadcast_capsule"):
-    print("Featured game")
-
-carousel = carousels[1]
-for link in carousel.findAll('a',class_="store_capsule daily_deal"):
-    #for gameLink in link.findAll():
-    print("Capsule game")
-    #for gameLink in link.findAll('a'):
-        #print(gameLink)
-        #gameLink = link.find('a',href=True)
-    #print(link.prettify())
-#print(carousel)
-#print(soup.prettify())
+# carousel = carousels[1]
+# for link in carousel.findAll('a',class_="store_capsule daily_deal"):
+#     #for gameLink in link.findAll():
+#     print("Capsule game")
+#     #for gameLink in link.findAll('a'):
+#         #print(gameLink)
+#         #gameLink = link.find('a',href=True)
+#     #print(link.prettify())
+# #print(carousel)
+# #print(soup.prettify())
